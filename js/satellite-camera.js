79b524ee170d9080a4ee2391dc363f7ec80739a1
Ray Villaraza <email@example.com>
2018-06-02 10:35:27 -0700

    Got the camera system up, now just need navigation systems to move forward

--- js/satellite-camera.js
+++ js/satellite-camera.js
@@ -2,62 +2,87 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const unknownWorldsOrientation = {
         landscape: [
-            '<img src="images/1018-l.jpg">',
-            '<img src="images/1095-l.jpg">',
-            '<img src="images/1104-l.jpg">',
-            '<img src="images/1325-l.jpg">',
-            '<img src="images/1548-l.jpg">',
-            '<img src="images/1716-l.jpg">',
-            '<img src="images/2047-l.jpg">',
-            '<img src="images/2063-l.jpg">',
-            '<img src="images/6344-l.jpg">',
-            '<img src="images/6590-l.jpg">',
+            'images/1018-l.jpg',
+            'images/1095-l.jpg',
+            'images/1104-l.jpg',
+            'images/1325-l.jpg',
+            'images/1548-l.jpg',
+            'images/1716-l.jpg',
+            'images/2047-l.jpg',
+            'images/2063-l.jpg',
+            'images/6344-l.jpg',
+            'images/6590-l.jpg',
         ],
         portrait: [
-            '<img src="images/1018-e.jpg">',
-            '<img src="images/1095-e.jpg">',
-            '<img src="images/1104-e.jpg">',
-            '<img src="images/1325-e.jpg">',
-            '<img src="images/1548-e.jpg">',
-            '<img src="images/1716-e.jpg">',
-            '<img src="images/2047-e.jpg">',
-            '<img src="images/2063-e.jpg">',
-            '<img src="images/6344-e.jpg">',
-            '<img src="images/6590-e.jpg">',
+            'images/1018-e.jpg',
+            'images/1095-e.jpg',
+            'images/1104-e.jpg',
+            'images/1325-e.jpg',
+            'images/1548-e.jpg',
+            'images/1716-e.jpg',
+            'images/2047-e.jpg',
+            'images/2063-e.jpg',
+            'images/6344-e.jpg',
+            'images/6590-e.jpg',
         ]
     }
-    const planetarySurfaceDisplay = document.getElementById('planetary-surface');
+    const planetSurface = document.getElementById('planet-surface');
+    const planetSurfaceDisplay = document.getElementById('planet-surface-image');
     const selectStarSystem = Math.floor(Math.random() * Math.floor(10));
     var unknownWorlds = null;
+
+    window.addEventListener('resize', function(e) {
+        cameraOrientation();
+    });
+
+    var chosenWorld = function() {
+        return unknownWorlds[selectStarSystem];
+    }
 
     function cameraOrientation() {
 
         if (window.innerHeight > window.innerWidth) {
 
-            planetarySurfaceDisplay.className = 'portrait';;
+            planetSurface.className = 'portrait';
             unknownWorlds = unknownWorldsOrientation["portrait"];
 
         } else {
 
-            planetarySurfaceDisplay.className = 'landscape';
+            planetSurface.className = 'landscape';
             unknownWorlds = unknownWorldsOrientation["landscape"];
         }
 
         missionDisplay();
     }
 
-    var chosenWorld = function() {
-        return unknownWorlds[selectStarSystem];
+    function missionDisplay() {
+
+        planetSurfaceDisplay.src = chosenWorld();
+
+        planetSurfaceDisplay.addEventListener('load', function() {
+            adjustCameraResolution();
+        })
     }
 
-    function missionDisplay() {
-        planetarySurfaceDisplay.innerHTML = chosenWorld();
+    function adjustCameraResolution() {
+
+        let monitor = planetSurfaceDisplay.parentNode;
+        let monitorHeight = monitor.offsetHeight;
+        let monitorWidth = monitor.offsetWidth;
+
+        let imageHeight = planetSurfaceDisplay.height;
+        let imageWidth = planetSurfaceDisplay.width;
+
+        // console.log('The monitor container is', monitorWidth, monitorHeight, '. The image is', imageWidth, imageHeight);
+
+        if (imageWidth < monitorWidth) {
+            planetSurfaceDisplay.className = 'fill-width';
+        }
+
+        if (imageHeight < monitorHeight) {
+            planetSurfaceDisplay.className = 'fill-height';
+        }
     }
 
     cameraOrientation();
-    missionDisplay();
-
-    window.addEventListener('resize', function(e) {
-        cameraOrientation();
-    });
 })
